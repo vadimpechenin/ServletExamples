c8421cd33d9ed9d8410e9175cb06dd25828d2ed9
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2023-02-17 10:48:04 +0400

    United post and get to one servlet

--- ExamplesGetPost/src/main/java/com/getpost/MainServlet.java
+++ ExamplesGetPost/src/main/java/com/getpost/MainServlet.java
@@ -1,8 +1,6 @@
 package com.getpost;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,12 +9,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 //@WebServlet(name = "ColorGetServlet", urlPatterns = "/colorget")
-public class ColorGetServlet extends HttpServlet{
+public class MainServlet extends HttpServlet{
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String color = request.getParameter("color");
         response.setContentType("text/html");
         PrintWriter pw = response.getWriter();
-        pw.println("<B>The selected color is: ");  //Выбранный цвет
+        pw.println("<B>Get: The selected color is: ");  //Выбранный цвет
+        pw.println(color);
+        pw.close();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String color = request.getParameter("color");
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<B>Post: The selected color is: ");  //Выбранный цвет
         pw.println(color);
         pw.close();
     }
